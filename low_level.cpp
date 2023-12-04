1995081c4d5dae9cbc197fec8fb62478812f357a
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2023-12-04 15:24:33 +0100

    Build all project and code a basic function in C++

---
+++ low_level.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Affiche un message simple
+    std::cout << "Bonjour, ceci est un programme C++ basique." << std::endl;
+
+    // Retourne 0 pour indiquer une exécution réussie
+    return 0;
+}
