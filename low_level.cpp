b2b5a228c9bb711370d981bd5ac54c8b219f3d7b
BaptisteRamonda <71442501+BaptisteRamonda@users.noreply.github.com>
2023-12-20 09:22:25 +0100

    Modifications

--- low_level.cpp
+++ low_level.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main() {
-    // Affiche un message simple
-    std::cout << "Hello world." << std::endl;
+    // URL du serveur local
+    std::string url = "http://localhost:8000/"; // Remplacez "port" par le port de votre serveur et "endpoint" par le point d'accès de votre API
 
-    // Retourne 0 pour indiquer une exécution réussie
+    // Effectuer une requête GET
+    cpr::Response response = cpr::Get(cpr::Url{url});
+
+    // Vérifier si la requête a réussi
+    if (response.status_code == 200) {
+        // Le contenu de la réponse est stocké dans response.text
+        std::cout << "Réponse du serveur : " << response.text << std::endl;
+    } else {
+        // En cas d'échec de la requête, afficher le code d'erreur et le message
+        std::cerr << "Erreur de requête : " << response.status_code << " - " << response.error.message << std::endl;
+    }
+
     return 0;
 }
